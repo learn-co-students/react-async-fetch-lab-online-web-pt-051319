72de47eba18689e8466e727396ed5d38d7825f45
jasmine1226 <jasmine1226@gmail.com>
2020-02-15 14:57:10 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { Component } from "react";
+
+export default class App extends Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then(res => res.json())
+      .then(json => {
+        let people = json.people.map(p => p.name);
+        this.setState({ data: people });
+      });
+  }
+  render() {
+    return (
+      <p>
+        {this.state.data.map(p => (
+          <p>{p}</p>
+        ))}
+      </p>
+    );
+  }
+}
